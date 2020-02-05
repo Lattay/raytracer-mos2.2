d63d7e63a57de397a009909546597c7a5600b969
Theo Cavignac <theo.cavignac@ecl16.ec-lyon.fr>
2020-02-05 10:02:47 +0100

    Add a radius property to light

--- src/Light.hpp
+++ src/Light.hpp
@@ -6,15 +6,16 @@ class Light {
   private:
     Vec m_source;
     Vec m_color;
-    double m_intensity;
+    double m_radius, m_intensity;
 
   public:
-    Light(Vec v, double i):
-      m_source(v), m_color(Vec(1, 1, 1)), m_intensity(i) {};
-    Light(Vec v, double i, Vec color):
-      m_source(v), m_color(color), m_intensity(i) {};
+    Light(Vec v, double i, double r):
+      m_source(v), m_color(Vec(1, 1, 1)), m_radius(r), m_intensity(i) {};
+    Light(Vec v, double i, double r, Vec color):
+      m_source(v), m_color(color), m_radius(r), m_intensity(i) {};
     Vec source() const{return m_source;};
     double intensity() const{return m_intensity;};
+    double radius() const{return m_radius;};
 };
 
 #endif
